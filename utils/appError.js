3623acf00efecd0f8d2334353359aa89438d78b6
xinwei0323 <32288170+xinwei0323@users.noreply.github.com>
2021-05-13 23:55:28 +0800

    add appError.js

---
+++ utils/appError.js
@@ -0,0 +1,16 @@
+class AppError extends Error {
+    constructor(message, statusCode) {
+        super(message);
+        this.statusCode = statusCode;
+        this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+        this.isOperational = true;
+
+        Error.captureStackTrace(this, this.constructor);
+
+        // 如果没有向captureStackTrace传递参数this.constructor，则在访问.stack属性时，
+        // this.constructor及其内部信息将会出现在堆栈信息中。
+        // 当传递this.constructor参数时，这些信息会被忽略。
+    }
+}
+
+console.log(new AppError('stupid', 404));
